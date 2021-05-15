c53af6db6a2a1cc0ca71a3d7614ef9dafa438679
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-05-15 12:34:21 +0100

    ♻ Connect subcribe form to backend

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,5 @@
 const days = document.querySelector(".days");
+const formBtn = document.querySelector(".subscribe-btn");
 
 const launchDate = new Date("10/4/2021");
 const today = new Date();
@@ -6,6 +7,26 @@ const oneDay = 1000 * 60 * 60 * 24;
 const daysLeft = Math.ceil((launchDate.getTime() - today.getTime()) / oneDay);
 
 days.textContent = daysLeft;
+
+formBtn.addEventListener("click", async function (e) {
+  const formInput = document.querySelector("#email").value;
+
+  e.preventDefault();
+
+  await axios
+    .post("https://spasora-api.herokuapp.com/api/v1/subscribes", {
+      email: formInput,
+    })
+    .then(function (response) {
+      if (response.data.status === "success") {
+        document.querySelector(".form").style.display = "none";
+        document.querySelector(".thanks").style.display = "block";
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
 
 if ((today.getHours() = 24)) {
   location.reload();
